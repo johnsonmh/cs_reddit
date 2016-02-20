8c716054ea061a2293cac4b0bab56a6748932b6e
MH Johnson <michaelhenry.johnson@gmail.com>
2016-02-20 16:19:44 +0100

    load list of companies into memory

--- list_of_companies.py
+++ list_of_companies.py
@@ -1,2 +1,13 @@
-def getListOfCompanies():
-	
+def loadListOfCompanies():
+	companiesCsv = file("The Really Big Hugely Ginormous Tech Company List - Sheet1.csv")
+	companyNames = []
+	for line in companiesCsv:
+		company = line.split(",")[1]
+		companyNames.append(company)
+	companiesCsv.close()
+	return set(companyNames) #remove duplicates		
+
+
+def main():
+	companies = loadListOfCompanies()
+
