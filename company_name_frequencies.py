d67b0b8d4aa6dd2862c7ec1539d9de564255b7e1
MH Johnson <michaelhenry.johnson@gmail.com>
2016-04-24 13:58:48 +0200

    re stylized to snake_case for consistency

--- company_name_frequencies.py
+++ company_name_frequencies.py
@@ -1,65 +1,63 @@
 import praw
-from collections import defaultdict
 import time
 
-all_companies = []
 already_seen_posts = []
-company_counts = defaultdict(int)
 
-def loadListOfCompanies():
-	global all_companies
-	companiesCsv = file("The Really Big Hugely Ginormous Tech Company List - Sheet1.csv")
-	companyNames = []
-	for line in companiesCsv:
-		company = line.split(",")[1].lower()
-		companyNames.append(company)
-	companiesCsv.close()
-	all_companies = list(set(companyNames)) #remove duplicates	
+def load_list_of_companies():
+    companies_csv = file("The Really Big Hugely Ginormous Tech Company List - Sheet1.csv")
+    company_names = []
+    for line in companies_csv:
+        company = line.split(",")[1].lower()
+        company_names.append(company)
+    companies_csv.close()
+    all_companies = list(set(company_names)) #remove duplicates  
+    return all_companies
 
-def fetchPosts():
-	global already_seen_posts
-	reddit = praw.Reddit(user_agent='cscareerquestions_company_mentions')
-	cscq = reddit.get_subreddit('cscareerquestions')
-	posts = cscq.get_top_from_all(limit="none")
-	newPosts = []
-	
-	for post in posts: #go through every post
-		if post not in already_seen_posts:
-			newPosts.append(post)
-			already_seen_posts.append(post)
-	return newPosts
-	
-def searchPostForCompanies(posts):
-	global company_counts
-	for post in posts:
-		wordsInPost = post.selftext.split() + post.title.split()
-		for word in wordsInPost: #go through every word in the comment
-			word = tokenizeWord(word)
-			if word in all_companies:
-				company_counts[word] += 1
+def fetch_posts():
+    global already_seen_posts
+    reddit = praw.Reddit(user_agent='cscareerquestions_company_mentions')
+    cscq = reddit.get_subreddit('cscareerquestions')
+    posts = cscq.get_top_from_all(limit="none")
+    newPosts = []
+    
+    for post in posts: #go through every post
+        if post not in already_seen_posts:
+            newPosts.append(post)
+            already_seen_posts.append(post)
+    return newPosts
+    
+def search_post_for_companies(posts, all_companies, company_counts):
+    for post in posts:
+        words_in_post = post.selftext.split() + post.title.split()
+        for word in words_in_post: #go through every word in the comment
+            word = tokenize_word(word)
+            if word in all_companies:
+                company_counts[word] = company_counts.get(word, 0) + 1
 
-def tokenizeWord(word):
-	#convert from unicode to ascii
-	word = word.encode('ascii','ignore')
-	punctuation = [".",",","!","?",'"']
-	#remove punctiation
-	tokenizedWord = ''.join(char for char in word if char not in punctuation)
-	tokenizedWord = tokenizedWord.lower()
-	#if possesive, drop the 's
-	tokenizedWord = tokenizedWord.replace("'s","")
-	return tokenizedWord
+def tokenize_word(word):
+    #convert from unicode to ascii
+    word = word.encode('ascii','ignore')
+    punctuation = [".",",","!","?",'"']
+    #remove punctiation
+    tokenized_word = ''.join(char for char in word if char not in punctuation)
+    tokenized_word = tokenized_word.lower()
+    #if possesive, drop the 's
+    tokenized_word = tokenized_word.replace("'s","")
+    return tokenized_word
 
 def main():
-	global company_counts
-	loadListOfCompanies()
-	while True:
-		newPosts = fetchPosts()
-		if len(newPosts) > 0:
-			searchPostForCompanies(newPosts)
-			print company_counts
-		else:
-			print "nothing new"
-		time.sleep(2)
+    all_companies = load_list_of_companies()
+    company_counts = {}
+    while True:
+        new_posts = fetch_posts()
+        if len(new_posts) > 0:
+            search_post_for_companies(new_posts, all_companies, company_counts)
+            print company_counts
+        else:
+            print "nothing new"
+        time.sleep(2)
 
-main()
+if __name__ == "__main__":
+    main()
+
 
